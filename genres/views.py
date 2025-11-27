9f0c93b1a4d3c79cac13ee7a3c3f438f2ee3d109
Ronaldo-Carlos <ronaldocarlos@outlook.com>
2025-11-27 07:46:49 -0300

    Criado serializers e ajustado views.py

--- genres/views.py
+++ genres/views.py
@@ -5,18 +5,12 @@ from genres.models import Genre
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework import generics
+from genres.serializers import GenreSerializer
 
-@csrf_exempt
-def genre_create_list_view(request):
-    if request.method == 'GET':
-        genres = Genre.objects.all()
-        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-        return JsonResponse(data, safe=False)
-    elif request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8'))
-        new_genre = Genre.objects.create(name=data['name'])
-        new_genre.save()
-        return JsonResponse({'id': new_genre.id, 'name': new_genre.name}, status=201)
+
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
 
 
 
